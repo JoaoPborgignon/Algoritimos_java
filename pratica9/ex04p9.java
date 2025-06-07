88e549003011918da5822d4bd0f9243c2abccf5c
Joao Pedro Bordignon <joaopebordignon@gmail.com>
2025-06-06 21:25:20 -0300

    Pratica 9

---
+++ pratica9/ex04p9.java
@@ -0,0 +1,32 @@
+package pratica9;
+
+public class ex04p9 {
+    
+}
+
+ interface CapazDeAssobiar {
+    public void assobiar(String assobio);
+    
+}
+
+public class Humano implements CapazDeAssobiar {        
+    public void assobiar(String assobio){
+        System.out.println(assobio);
+    }        
+}
+
+/*
+ * A classe Humano abaixo implementa uma interface chamada CapazDeAssobiar.
+
+public class Humano implements CapazDeAssobiar {        
+    public void assobiar(String assobio){
+        System.out.println(assobio);
+    }        
+}
+
+No entanto, essa interface não foi declarada no programa Java. Declare a interface CapazDeAssobiar para atender o caso de teste abaixo.
+
+Observação:
+
+ - declare a interface sem modificador de acesso (ou seja, sem public, private ou protected antes da palavra interface).
+ */
